c17710ab151789da4f54b9d6dd0e0c3d1cfea9e8
Alexis Paleta <157434896+AlexisPaleta@users.noreply.github.com>
2025-04-06 19:36:55 -0600

    Binding implementation on portrait calculator fragments

--- app/src/main/java/com/fcc/calculadora/BasicOperationsFragment.kt
+++ app/src/main/java/com/fcc/calculadora/BasicOperationsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fcc.calculadora.databinding.FragmentBasicOperationsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +21,8 @@ class BasicOperationsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentBasicOperationsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +37,8 @@ class BasicOperationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basic_operations, container, false)
+        _binding = FragmentBasicOperationsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
--- app/src/main/java/com/fcc/calculadora/ResultsFragment.kt
+++ app/src/main/java/com/fcc/calculadora/ResultsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fcc.calculadora.databinding.FragmentResultsBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +17,12 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ResultsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ResultsFragment : Fragment() {
+class ResultsFragment : Fragment() { //This fragment is for the basic calculator functionality
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding: FragmentResultsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +37,8 @@ class ResultsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_results, container, false)
+        _binding = FragmentResultsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
